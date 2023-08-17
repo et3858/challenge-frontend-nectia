cf309c4c1d8bbb972588d0f7341519c391d8fbb4
Eduardo Tejada <et3858@gmail.com>
2023-08-17 12:30:41 -0400

    Apply table sorting, filtering and pagination

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,17 +1,73 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useMemo, useState } from "react";
+import {
+    flexRender,
+    getCoreRowModel,
+    getFilteredRowModel,
+    getPaginationRowModel,
+    getSortedRowModel,
+    useReactTable,
+} from '@tanstack/react-table';
 import { UserContext } from "../contexts/UserContext";
 import LoadingElement from "../components/LoadingElement";
+import InputElement from "../components/InputElement";
 
 
 // books-challenge-nectia
 const MOCK_URL = "https://run.mocky.io/v3/13768651-741e-4d2a-9f5e-61f4282f7d3e";
 
 
+const convertToHumanDate = (props) => (
+    // From the UTC string format, extract only the day, month and year and concat them
+    new Date(`${props.getValue()}T00:00:00`)
+        .toUTCString()
+        .split(" ")
+        .splice(1, 3)
+        .join(" ")
+);
+
+
+const PAGE_SIZES = [2, 5, 10];
+const COLUMNS = [
+    { header: 'Title', accessorKey: 'title' },
+    { header: 'Author', accessorKey: 'author' },
+    { header: 'Genre', accessorKey: 'genre' },
+    {
+        header: 'Publication date',
+        accessorKey: 'publication_date',
+        enableGlobalFilter: false,
+        cell: convertToHumanDate,
+    },
+];
+
+
 function Home() {
     const userDetails = useContext(UserContext);
+
     const [books, setBooks] = useState([]);
     const [loading,  setLoading] = useState(true);
     const [showBooks, setShowBooks] = useState(false);
+    const [sorting, setSorting] = useState([]);
+    const [filtering, setFiltering] = useState('');
+    const [{ pageIndex, pageSize }, setPagination] = useState({ pageIndex: 0, pageSize: PAGE_SIZES[1] });
+    const pagination = useMemo(() => ({ pageIndex, pageSize }), [pageIndex, pageSize]);
+
+
+    const table = useReactTable({
+        columns: COLUMNS,
+        data: books,
+        getCoreRowModel: getCoreRowModel(),
+        getSortedRowModel: getSortedRowModel(),
+        getPaginationRowModel: getPaginationRowModel(),
+        getFilteredRowModel: getFilteredRowModel(),
+        state: {
+            sorting: sorting,
+            globalFilter: filtering,
+            pagination,
+        },
+        onSortingChange: setSorting,
+        onGlobalFilterChange: setFiltering,
+        onPaginationChange: setPagination,
+    });
 
 
     const handleEditClick = (e) => {
@@ -28,16 +84,6 @@ function Home() {
             setBooks(booksCopy);
         }
     };
-
-
-    const convertToHumanDate = (date) => (
-        // From the UTC string format, extract only the day, month and year and concat them
-        new Date(`${date}T00:00:00`)
-            .toUTCString()
-            .split(" ")
-            .splice(1, 3)
-            .join(" ")
-    );
 
 
     useEffect(() => {
@@ -60,52 +106,128 @@ function Home() {
     }, []);
 
 
+    const renderCellHeader = (header) => {
+        return (
+            <th
+                key={header.id}
+                onClick={header.column.getToggleSortingHandler()}
+                className="border-y border-slate-600 p-2"
+            >
+                {header.isPlaceholder ? null : (
+                    <div>
+                        {flexRender(header.column.columnDef.header, header.getContext())}
+
+                        {{ asc: '🔼', desc: '🔽' }[header.column.getIsSorted() ?? null]}
+                    </div>
+                )}
+            </th>
+        );
+    };
+
+
     return (
         <>
             {loading ? <LoadingElement /> : null}
 
             {showBooks ?
+                <>
+                <div className="flex flex-row gap-4 items-center mb-2">
+                    <div className="flex flex-row items-center gap-2 mb-2">
+                        <button
+                            type="button"
+                            className="bg-gray-500 rounded-full flex-none"
+                            onClick={() => table.setPageIndex(0)}
+                            disabled={!table.getCanPreviousPage()}
+                        >{'|<'}</button>
+                        <button
+                            type="button"
+                            className="bg-gray-500 rounded-full flex-none"
+                            onClick={() => table.previousPage()}
+                            disabled={!table.getCanPreviousPage()}
+                        >{'<'}</button>
+
+                        <span>
+                            Page&nbsp;
+                            <strong>{table.getState().pagination.pageIndex + 1}</strong>&nbsp;
+                            of&nbsp;
+                            <strong>{table.getPageCount()}</strong>
+                        </span>
+
+                        <button
+                            type="button"
+                            className="bg-gray-500 rounded-full flex-none"
+                            onClick={() => table.nextPage()}
+                            disabled={!table.getCanNextPage()}
+                        >{'>'}</button>
+                        <button
+                            type="button"
+                            className="bg-gray-500 rounded-full flex-none"
+                            onClick={() => table.setPageIndex(table.getPageCount() - 1)}
+                            disabled={!table.getCanNextPage()}
+                        >{'>|'}</button>
+                    </div>
+
+                    <div className="flex-none">
+                        <select
+                            className="border cursor-pointer rounded-xl w-full p-2 bg-gray-700 text-whote"
+                            value={table.getState().pagination.pageSize}
+                            onChange={e => table.setPageSize(Number(e.target.value))}
+                        >
+                            {PAGE_SIZES.map(pageSize => (
+                                <option key={pageSize} value={pageSize}>Show {pageSize}</option>
+                            ))}
+                        </select>
+                    </div>
+                </div>
+
+                <div className="mb-2">
+                    <InputElement
+                        type='text'
+                        value={filtering}
+                        onChange={e => setFiltering(e.target.value)}
+                        placeholder="Search by title, author or genre"
+                    />
+                </div>
+
                 <table className="border-collapse border border-slate-500 border-spacing-2 text-left">
                     <thead>
-                        <tr>
-                            <th className="border-y border-slate-600 p-2">Title</th>
-                            <th className="border-y border-slate-600 p-2">Author</th>
-                            <th className="border-y border-slate-600 p-2">Genre</th>
-                            <th className="border-y border-slate-600 p-2">Publication date</th>
-                            <th className="border-y border-slate-600 p-2">Options</th>
-                        </tr>
+                        {table.getHeaderGroups().map(headerGroup => (
+                            <tr key={headerGroup.id}>
+                                {headerGroup.headers.map(renderCellHeader)}
+                                <th className="border-y border-slate-600 p-2">Options</th>
+                            </tr>
+                        ))}
                     </thead>
 
                     <tbody>
-                        {books.map((e, i) => (
-                            <tr key={e.id} className={i % 2 === 0 ? 'bg-slate-800' : ''}>
-                                <td className="border-y border-slate-600 p-2">{e.title}</td>
-                                <td className="border-y border-slate-600 p-2">{e.author}</td>
-                                <td className="border-y border-slate-600 p-2">{e.genre}</td>
-                                <td className="border-y border-slate-600 p-2">{convertToHumanDate(e.publication_date)}</td>
+                        {table.getRowModel().rows.map((row, i) => (
+                            <tr key={row.id} className={i % 2 === 0 ? 'bg-slate-600' : ''}>
+                                {row.getVisibleCells().map(cell => (
+                                    <td key={cell.id} className="border-y border-slate-600 p-2">
+                                        {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                                    </td>
+                                ))}
+
                                 <td className="border-y border-slate-600 p-2">
                                     <div className="flex gap-2">
                                         <button
                                             type="button"
-                                            className="bg-blue-400 text-white"
-                                            onClick={() => handleEditClick(e)}
-                                        >
-                                            Edit
-                                        </button>
+                                            className="bg-blue-400 text-white py-2"
+                                            onClick={() => handleEditClick(row.original)}
+                                        >Edit</button>
 
                                         <button
                                             type="button"
-                                            className="bg-red-500 text-white"
-                                            onClick={() => handleDeleteClick(e)}
-                                        >
-                                            Delete
-                                        </button>
+                                            className="bg-red-500 text-white py-2"
+                                            onClick={() => handleDeleteClick(row.original)}
+                                        >Delete</button>
                                     </div>
                                 </td>
                             </tr>
                         ))}
                     </tbody>
                 </table>
+                </>
             : null}
         </>
     );
